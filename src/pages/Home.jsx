ca416fa4660d968fbf8b0a97a48215ed8fdc144f
sms32 <sammichael@karunya.edu.in>
2025-07-18 10:41:02 +0530

    fixed the issues .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ParticleBackground from "../components/ParticleBackground";
-import Event from "./Events";
 import Team from "./Team";
 function App() {
   return (
     <div>
     <ParticleBackground />
-    <Event/>
     <Team/>
     </div>
   )
